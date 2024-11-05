501425af58a046dbdae5399f433c67b1088c8a2f
Harshit Soni <harshit11664@gmail.com>
2024-11-05 23:36:19 +0530

    Added Day 73 to 81

--- MS_Reward_Points_Searches.py
+++ MS_Reward_Points_Searches.py
@@ -6,56 +6,13 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from pytrends.request import TrendReq
 from selenium import webdriver
-from tqdm import tqdm
 import random
 import time
 
 
-class MS_Account:
-    def __init__(self, close_edge: bool = False, no_of_searches: int = 33):
-        self.edge_options = webdriver.EdgeOptions()
-        self.edge_options.add_experimental_option("detach", True)
-        self.driver = webdriver.Edge(options=self.edge_options)
-        self.URL = "https://www.bing.com/?scope=web&cc=IN&FORM=ANNTH1&pc=PCMWSB"
-        self.Element_XPATH = '/html/body/div[1]/div/div[3]/div[2]/form/div[1]/div/textarea'
-        self.MS_REWARD_URL = "https://rewards.bing.com/"
-        self.no_of_searches = no_of_searches
-        self.list_of_topics = self.list_of_searches()
-        self.sleep_time: int = 2
-        self.tab_close_time: int = 5
-        self.browser_close_time: int = 5
-        self.open_reward_page: bool = not close_edge
-        self.close_edge: bool = close_edge
-
-    def start_searches(self):
-        print(f"Starting Searches")
-        time.sleep(5)
-        self.loop_searches()
-        if self.open_reward_page:
-            print("Opening MS Reward Page")
-            self.driver.get(self.MS_REWARD_URL)
-        if self.close_edge:
-            print("Closing Edge in 15 Seconds...")
-            time.sleep(15)
-            self.driver.close()
-
-    def loop_searches(self):
-        try:
-            for topic in tqdm(self.list_of_topics, desc="Searching Topics"):
-                self.driver.get(self.URL)
-                search_element = self.driver.find_element(By.XPATH, value=self.Element_XPATH)
-                search_element.send_keys(topic)
-                time.sleep(self.sleep_time)
-                search_element.send_keys(Keys.ENTER)
-                time.sleep(self.tab_close_time)
-            time.sleep(self.sleep_time)
-        except:
-            self.driver.close()
-            print("Previous Attempt Failed! Retrying !!......")
-            self.loop_searches()
-        print("Done Searches")
-
-    def list_of_searches(self):
+def list_of_trending_topics():
+    retries = 3
+    while retries > 0:
         try:
             pytrends = TrendReq(hl='en-US', tz=360)
             trending_searches_df_top50 = pytrends.realtime_trending_searches(pn='IN')
@@ -69,14 +26,67 @@ class MS_Account:
             seen = set()
             unique_list = [x for x in entity_list if x not in seen and not seen.add(x)]
             random.shuffle(unique_list)
-            if self.no_of_searches <= len(unique_list):
-                return unique_list[:self.no_of_searches]
-            else:
-                return unique_list
-        except:
-            print(
-                f"Previous Attempt to search {self.no_of_searches} topics Failed! Retrying !!..... Please Check the Internet......")
-            self.list_of_searches()
+            print("\n---- Done Searching Topics ----\n")
+            return unique_list
+        except Exception as exception:
+            print(f"Previous Attempt to search topics Failed! Retrying !!..... Please Check the Internet......")
+            print(f"Error: {exception}. \nRetrying in 2 seconds...")
+            time.sleep(2)
+            retries -= 1
+    print("Failed to retrieve trending searches after multiple attempts.")
+    return ['NFT', 'Hanukkah', 'Crossword', 'Minecraft', 'Park Gelled', 'Christmas', 'NHL', 'iPhone 13', 'Barbie',
+            'Fortnite', 'Gambling', 'The Last of Us', 'Cyber+Monday', 'Among+Us', 'Golf', 'Web3', 'New+Year',
+            'Cricket World Cup', 'Louvre Museum', 'Omicron', 'Diwali', 'Black+Friday', 'Scrabble', 'Kwanzaa', 'Chess',
+            'Taylor Swift', 'Olympics', 'Adele', 'MLB', 'Thanksgiving', 'Squid Game', 'Roblox', 'Metaverse', 'Amazon',
+            'Facebook', 'Tennis', 'Tesla', 'Gmail', 'Google', 'Paralympics', 'MotoGP', 'Sudoku',
+            'Spider-Man: No Way Home', 'NBA', 'UEFA+Champions+League', 'YouTube', 'Weather', 'FIFA+World+Cup',
+            'Formula+1', 'Subhuman Gill', 'Halloween', 'Netflix', 'Caterpillar', 'Bones', 'Bitcoin', 'NFL']
+
+
+class MS_Account:
+    def __init__(self):
+        self.edge_options = webdriver.EdgeOptions()
+        self.edge_options.add_experimental_option("detach", True)
+        self.driver = webdriver.Edge(options=self.edge_options)
+        self.URL = "https://www.bing.com/?scope=web&cc=IN&FORM=ANNTH1&pc=PCMWSB"
+        self.Element_XPATH = '/html/body/div[1]/div/div[3]/div[2]/form/div[1]/div/textarea'
+        self.MS_REWARD_URL = "https://rewards.bing.com/"
+        self.sleep_time: int = 2
+        self.tab_close_time: int = 4
+        self.browser_close_time: int = 5
+
+    def start_searches(self, topics_list):
+        print(f"Search Topics are :- {topics_list}\n")
+        print(f"---- Starting Searches ----")
+        for topic in topics_list:
+            self.search_topic(topic)
+
+    def close_browser(self):
+        print("Closing Edge in 5 Seconds...")
+        time.sleep(5)
+        self.driver.close()
+
+    def open_rewards(self):
+        print("Opening MS Reward Page")
+        self.driver.get(self.MS_REWARD_URL)
+
+    def search_topic(self, topic):
+        try:
+            self.driver.get(self.URL)
+            search_element = self.driver.find_element(By.XPATH, value=self.Element_XPATH)
+            search_element.send_keys(topic)
+            time.sleep(self.sleep_time)
+            search_element.send_keys(Keys.ENTER)
+            time.sleep(self.tab_close_time)
+            # print(f"Done Search:- {topic}")
+        except Exception as search_error:
+            retries = 3
+            while retries > 0:
+                print(f"Error: {search_error}\n... Retrying in 2 seconds ...")
+                self.driver.close()
+                self.search_topic(topic)
+                time.sleep(2)
+                retries -= 1
 
 
 if __name__ == '__main__':
@@ -88,16 +98,39 @@ if __name__ == '__main__':
 
     accounts = int(input("How many accounts do you want to do Task:- "))
     close_browser = bool(input("Do you want to close the browsers after all searches (1: True, 0: False):- "))
-    no_of_search_topics = int(input("How many Searches do you want to do in each account:- "))
+    no_of_search_topics = int(input("How many Searches do you want to do in each account ( < 100 topics):- "))
+
     browsers = []
+    if no_of_search_topics > 1:
+        TRENDING_SEARCHES = list_of_trending_topics()
+    else:
+        TRENDING_SEARCHES = []
+
+    # Opening Browsers
+    try:
+        for account in range(accounts):
+            print(f"\n-- Opening Browser {account + 1} --\n")
+            browsers.append(MS_Account())
+            print("You have 10 seconds to choose Account\n")
+            time.sleep(5)
+    except Exception as e:
+        print(f"\nError in opening the Browser. {e}\n")
 
     for account in range(accounts):
-        print(f"Opening Browser {account}")
-        browsers.append(MS_Account(close_browser, no_of_search_topics))
+        try:
+            print(f"\n\n-- Doing Searches for Account number {account + 1} --\n")
+            random.shuffle(TRENDING_SEARCHES)
+            search_list = TRENDING_SEARCHES[:no_of_search_topics]
+            print(search_list)
+            browsers[account].start_searches(search_list)
+            if not close_browser:
+                browsers[account].close_browser()
+            else:
+                browsers[account].open_rewards()
+            print(f"\n____ Done for Account number {account + 1} ____\n")
+        except Exception as e:
+            print(f"Error in searching in Account number {account + 1}")
+            print(f"Error is {e}")
+            browsers[account].close_browser()
 
-    for account in range(accounts):
-        print(f"Doing Searches for Account number {account}")
-        browsers[account].start_searches()
-        print(f"Done for Account number {account}")
-
-    print("************ Thank you for Using ************")
+    print("\n\n************ Thank you for Using ************\n\n")
